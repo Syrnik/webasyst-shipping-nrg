811143ea65fe9cd6f8424ba7b7e0c8b521efaf74
Serge Rodovnichenko <serge@syrnik.com>
2017-01-30 07:21:56 +0300

    Начальные конфиги

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,8 +1,9 @@
 <?php
-return array (
-  'name' => 'ТК Энергия',
-  'description' => '',
-  'icon' => 'img/nrg.png',
-  'version' => '1.0.0',
-  'vendor' => '670917',
+return array(
+    'name'        => 'ТК Энергия',
+    'description' => 'Расчет стоимости доставки транспортной компанией Энергия',
+    'icon'        => 'img/nrg16.png',
+    'logo'        => 'img/nrg.png',
+    'version'     => '1.0.0',
+    'vendor'      => '670917',
 );

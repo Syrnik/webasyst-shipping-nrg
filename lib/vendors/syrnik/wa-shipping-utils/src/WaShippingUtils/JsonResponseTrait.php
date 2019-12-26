fff869be77b47c7d19b597db6b8b9c6cfd907f82
Serge Rodovnichenko <sergerod@gmail.com>
2019-12-26 10:35:35 +0300

    Обновил библиотеку

--- lib/vendors/syrnik/wa-shipping-utils/src/WaShippingUtils/JsonResponseTrait.php
+++ lib/vendors/syrnik/wa-shipping-utils/src/WaShippingUtils/JsonResponseTrait.php
@@ -15,6 +15,7 @@ trait JsonResponseTrait
 {
     /**
      * @param mixed $data
+     * @throws \waException
      */
     protected function sendJsonData($data)
     {
@@ -27,18 +28,18 @@ trait JsonResponseTrait
 
     /**
      * @param mixed $response
+     * @throws \waException
      */
     protected function sendJsonResponse($response)
     {
-        if (\waRequest::isXMLHttpRequest()) {
-            wa()->getResponse()->addHeader('Content-Type', 'application/json')->sendHeaders();
-        }
+        wa()->getResponse()->addHeader('Content-Type', 'application/json')->sendHeaders();
         echo \waUtils::jsonEncode($response);
         exit;
     }
 
     /**
      * @param mixed $error
+     * @throws \waException
      */
     protected function sendJsonError($error)
     {
